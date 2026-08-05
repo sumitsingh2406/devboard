450c0e6408c506ffc94d023b3b82503c0bd31fe9
sumitsingh <sumitsingh2406@github.com>
2026-08-05 16:47:19 +0530

    "new dockerfile for backend and added to github"

--- frontend/src/components/layout/ProfileMenu.jsx
+++ frontend/src/components/layout/ProfileMenu.jsx
@@ -98,7 +98,7 @@ export function ProfileMenu() {
             <MenuItem
               icon={IconExternalLink}
               label="GitHub repo"
-              onClick={() => { setOpen(false); window.open('https://github.com/LondheShubham153/devboard', '_blank'); }}
+              onClick={() => { setOpen(false); window.open('https://github.com/LondheSumit153/devboard', '_blank'); }}
             />
             <div className="my-1 h-px bg-ink-100 dark:bg-white/10" />
             <MenuItem
